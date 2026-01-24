e3ab00b6085473ab727416642cea0c472b5bfcdb
HarshRaut9945 <harshraut9945@gmail.com>
2026-01-25 01:43:30 +0530

    Add

---
+++ Rag App/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import os
+import asyncio
+import nest_asyncio
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
+from langchain_community.document_loaders import PyMuPDFLoader,TextLoader,Docx2txtLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains import retrieval_qa
+
+
+# ---- Fix for Streamlit + async gRPC ----
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+nest_asyncio.apply()
+# ----------------------------------------
+
+
+
+#step 1
+load_dotenv()
+
+llm=ChatGoogleGenerativeAI(model='gemini-2.5-flash',google_api_key=os.getenv('GOOGLE_API_KEY'))
+
+embeddings=GoogleGenerativeAIEmbeddings(model='models/embedding-001',google_api_key=os.getenv('GOOGLE_API_KEY'))
+
+# step 2: stramlit App
+st.set_page_config(page_title="RAG with LangChain", page_icon="🤖", layout="wide")
+st.title("📂 RAG Q&A with Gemini")
+
+uploaded_file=st.file_uploader("uploaded file...",type=['.pdf','.txt','.dock'])
+
+
+if uploaded_file:
+    with st.spinner('File Uploading...Please wait')
