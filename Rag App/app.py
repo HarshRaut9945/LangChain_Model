727002f9c32ac5ed6459e54c34540130f4ce2072
HarshRaut9945 <harshraut9945@gmail.com>
2026-01-25 02:36:01 +0530

    Add

--- Rag App/app.py
+++ Rag App/app.py
@@ -3,6 +3,8 @@ import os
 import asyncio
 import nest_asyncio
 from dotenv import load_dotenv
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 
 from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyMuPDFLoader,TextLoader,Docx2txtLoader,PyPDFLoader
@@ -10,6 +12,8 @@ from langchain_community.document_loaders import PyMuPDFLoader,TextLoader,Docx2t
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains import retrieval_qa
+from langchain.chains import RetrievalQA
+
 
 
 # ---- Fix for Streamlit + async gRPC ----
@@ -28,7 +32,11 @@ load_dotenv()
 
 llm=ChatGoogleGenerativeAI(model='gemini-2.5-flash',google_api_key=os.getenv('GOOGLE_API_KEY'))
 
-embeddings=GoogleGenerativeAIEmbeddings(model='models/embedding-001',google_api_key=os.getenv('GOOGLE_API_KEY'))
+# embeddings=GoogleGenerativeAIEmbeddings(model='models/embedding-001',google_api_key=os.getenv('GOOGLE_API_KEY'))
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
 
 # step 2: stramlit App
 st.set_page_config(page_title="RAG with LangChain", page_icon="🤖", layout="wide")
@@ -47,8 +55,8 @@ if uploaded_file:
 
         # Rag Step
 
-        #step 1
-           # Load file
+        #step 1 Load file
+          
         if uploaded_file.name.endswith(".pdf"):
             loader = PyPDFLoader(file_path)
         elif uploaded_file.name.endswith(".docx"):
@@ -60,3 +68,25 @@ if uploaded_file:
         # step 2:
         splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
         chunks=splitter.split_documents(docs)
+
+        # step 3 Embedding and Vector store
+        vectorestores=FAISS.from_documents(chunks,embeddings)
+        retriver=vectorestores.as_retriever()
+
+    
+        # step 4: LLm Chain
+        qa=RetrievalQA.from_chain_type(llm=llm,retriever=retriver, return_source_documents=True)
+        
+        st.success("File Process....")
+
+
+        #user Querry
+        user_querry=st.text_input("user Querry")
+
+        if user_querry:
+            if st.button("Get Answer"):
+              with st.spinner("fetching  Answer... please wait"):
+                 response=qa(user_querry)
+                 st.subheader("Answer")
+                 st.write(response)
+
