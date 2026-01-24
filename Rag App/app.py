26873cfba34c7c4047b8dfc70c01c9a45320632c
HarshRaut9945 <harshraut9945@gmail.com>
2026-01-25 02:04:10 +0530

    Add

--- Rag App/app.py
+++ Rag App/app.py
@@ -5,7 +5,7 @@ import nest_asyncio
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI,GoogleGenerativeAIEmbeddings
-from langchain_community.document_loaders import PyMuPDFLoader,TextLoader,Docx2txtLoader
+from langchain_community.document_loaders import PyMuPDFLoader,TextLoader,Docx2txtLoader,PyPDFLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -38,4 +38,25 @@ uploaded_file=st.file_uploader("uploaded file...",type=['.pdf','.txt','.dock'])
 
 
 if uploaded_file:
-    with st.spinner('File Uploading...Please wait')
+    with st.spinner('File Uploading...Please wait'):
+        file_path=f"temp_{uploaded_file.name}"
+        with open(file_path,'wb') as f:
+            f.write(uploaded_file.read())
+
+
+
+        # Rag Step
+
+        #step 1
+           # Load file
+        if uploaded_file.name.endswith(".pdf"):
+            loader = PyPDFLoader(file_path)
+        elif uploaded_file.name.endswith(".docx"):
+            loader = Docx2txtLoader(file_path)
+        else:
+            loader = TextLoader(file_path)
+        docs=loader.load()
+
+        # step 2:
+        splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+        chunks=splitter.split_documents(docs)
