7b67a8e7af935344db8c8c4de453f18a33bb780c
HarshRaut9945 <harshraut9945@gmail.com>
2026-04-08 00:27:18 +0530

    add

--- Fast_Api/main.py
+++ Fast_Api/main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException
+import json 
+
 
 app=FastAPI()
 
+def load_data():
+    with open('patients.json','r') as f:
+       data=json.load(f)
+
+       return data
+
+
+
 @app.get("/")
 def  hello():
-    return{'message':'Hello World HArsh'}
+    return{'message':'Patient Management System API'}
+
+@app.get('/about')
+def about():
+    return {'message':'A fully functional API to manage your payient records'}
+
+
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
+
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str=Path(...,description='ID of the patient in the DB',example='P002')):
+    # Load all the patients
+    data=load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'patient not found'}
